42a05116644ba1c8389c31dd1d9b6ff0f55d84e2
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-08-28 01:05:55 +0300

    add data table component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,126 @@
+import DataTable from "@/components/DataTable";
+import { cn } from "@/lib/utils";
+import { TrendingDown, TrendingUp } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {
+    header: "Name",
+    cellClassName: "name-cell",
+    cell: (coin) => {
+      const item = coin.item
+
+      return (
+        <Link
+          href={`/coins/${item.id}`}
+        >
+          <Image
+            src={item.large}
+            alt={item.name}
+            width={36}
+            height={36}
+          />
+          {item.name}
+        </Link>
+      )
+    }
+  },
+  {
+    header: "24h Change",
+    cellClassName: "name-cell",
+    cell: (coin) => {
+      const item = coin.item
+
+      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0
+
+      return (
+        <div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
+          <p>
+            {
+              isTrendingUp ? (
+                <TrendingUp width={16} height={16} />
+              ) : (
+                <TrendingDown width={16} height={16} />
+              )
+            }
+            {
+              Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)
+            }
+          </p>
+        </div>
+      )
+    }
+  },
+  {
+    header: "Price",
+    cellClassName: "price-cell",
+    cell: (coin) => coin.item.data.price
+  }
+]
+
+const dummyData: TrendingCoin[] = [
+  {
+    item: {
+      id: 'bitcoin',
+      name: 'Bitcoin',
+      symbol: 'BTC',
+      market_cap_rank: 1,
+      thumb: '/logo.svg',
+      large: '/logo.svg',
+      data: {
+        price: 89113.00,
+        price_change_percentage_24h: {
+          usd: 2.5
+        }
+      }
+    }
+  }
+]
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main className="main-container">
+
+      <section className="home-grid">
+
+        <div id="coin-overview">
+
+          <div className="header">
+
             <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
+              src={'https://assets.coingecko.com/coins/images/1/large/bitcoin.png'}
+              alt="Bitcoin"
+              width={56}
+              height={56}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+            <div className="info">
+              <p>Bitcoin / BTC</p>
+              <h1>$89,113.00</h1>
+            </div>
+
+          </div>
+
         </div>
-      </main>
-    </div>
+
+        <p>Trending Coins</p>
+
+        <DataTable
+          data={dummyData}
+          columns={columns}
+          rowKey={(coin) => coin.item.id}
+          tableClassName="trending-coins-table"
+        />
+
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+
+        <p>Categories</p>
+
+      </section>
+
+    </main>
   );
 }
