2fa165154f08135f7809cd243afd31b830d972e0
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-08-30 01:07:57 +0300

    add props class names css styles names

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -34,7 +34,7 @@ const DataTable = <T,>({
                         columns.map((column, i) => (
                             <TableHead
                                 key={i}
-                                className={cn('bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5')}
+                                className={cn('bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5', headerCellClassName, column.headClassName)}
                             >
                                 {column.header}
                             </TableHead>
@@ -53,7 +53,7 @@ const DataTable = <T,>({
                                 columns.map((column, columnIndex) => (
                                     <TableCell
                                         key={columnIndex}
-                                        className={cn('py-4 first:pl-5 last:pr-5')}
+                                        className={cn('py-4 first:pl-5 last:pr-5', bodyCellClassName, column.cellClassName)}
                                     >
                                         {
                                             column.cell(row, rowIndex)
