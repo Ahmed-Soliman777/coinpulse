7197c443ed9d8c479fd85ec78dcf2e6c515eace4
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-09-03 01:50:48 +0300

    add new things

---
+++ components/LiveDataWrapper.tsx
@@ -0,0 +1,101 @@
+"use client"
+
+import { useCoinGeckoWebSocket } from "@/hooks/useCoinGeckoWebSocket"
+import CandlestackChart from "./CandlestackChart"
+import { Separator } from "./ui/separator"
+import DataTable from "./DataTable"
+import { formatCurrency, timeAgo } from "@/lib/utils"
+import { useState } from "react"
+import CoinHeader from "./CoinHeader"
+
+const LiveDataWrapper = ({
+    coin,
+    coinId,
+    poolId,
+    coinOHLCData,
+    children
+}: LiveDataProps) => {
+
+    const [liveInterval, setLiveInterval] = useState<'1s' | '1m'>('1s')
+
+    const { trades, ohlcv, price } = useCoinGeckoWebSocket({ coinId, poolId, liveInterval: "1m" })
+
+    const tradeColumns: DataTableColumn<Trade>[] = [
+        {
+            header: "Price",
+            cellClassName: "price-cell",
+            cell: (trade) => (trade.price ? formatCurrency(trade.price) : "-"),
+        },
+        {
+            header: "Amount",
+            cellClassName: "amount-cell",
+            cell: (trade) => trade.amount?.toFixed(4) ?? "-",
+        },
+        {
+            header: "Value",
+            cellClassName: "value-cell",
+            cell: (trade) => (trade.value ? formatCurrency(trade.value) : "-"),
+        },
+        {
+            header: "Buy/Sell",
+            cellClassName: "type-cell",
+            cell: (trade) => (
+                <span
+                    className={trade.type === "b" ? "text-green-500" : "text-red-500"}
+                >
+                    {trade.type === "b" ? "Buy" : "Sell"}
+                </span>
+            ),
+        },
+        {
+            header: "Time",
+            cellClassName: "time-cell",
+            cell: (trade) => (trade.timestamp ? timeAgo(trade.timestamp) : "-"),
+        },
+    ];
+    return (
+        <section id="live-data-wrapper">
+            <CoinHeader
+                name={coin.name}
+                image={coin.image.large}
+                livePrice={price?.usd ?? coin.market_data.current_price.usd}
+                livePriceChangePercentage24h={
+                    price?.volume24h ?? coin.market_data.price_change_percentage_30d_in_currency
+                }
+                priceChange24h={coin.market_data.price_change_24h_in_currency.usd}
+            />
+            <Separator className={'divider'} />
+
+            <div className="trend">
+                <CandlestackChart
+                    coinId={coinId}
+                    data={coinOHLCData}
+                    liveOhlcv={ohlcv}
+                    mode="live"
+                    initialPeriod="daily"
+                    liveInterval={liveInterval}
+                    setLiveInterval={setLiveInterval}
+                >
+                    <h4>Trend Overview</h4>
+                </CandlestackChart>
+            </div>
+
+            <Separator className={'divider'} />
+
+            {tradeColumns && (
+                <div className="trades">
+                    <h4>Recent Trades</h4>
+
+                    <DataTable
+                        columns={tradeColumns}
+                        data={trades}
+                        rowKey={(_, index) => index}
+                        tableClassName="trades-table"
+                    />
+                </div>
+            )}
+        </section>
+    )
+}
+
+export default LiveDataWrapper
